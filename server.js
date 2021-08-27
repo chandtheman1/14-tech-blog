d0c0ea5db0f40dc019f95ac0bdc89c7d06b0d48c
Chandler Liang <chandler.liang123@gmail.com>
2021-08-27 15:26:04 +1000

    set up the initial main.handlebars

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+const path = require('path');
 require('dotenv').config();
-
 const express = require('express');
-const app = express();
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
+const app = express();
 const PORT = process.env.PORT || 8080;
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-  });
+    res.render('homepage');
+});
    
 app.listen(PORT);
