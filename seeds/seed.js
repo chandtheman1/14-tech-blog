ca0a2ea4409ff607045cb7b6725f90f76804ee2c
Chandler Liang <chandler.liang123@gmail.com>
2021-09-01 14:12:48 +1000

    created comment data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User , Blog } = require('../models');
+const { User , Blog, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -13,6 +14,11 @@ const seedDatabase = async () => {
     });
 
     await Blog.bulkCreate(blogData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Comment.bulkCreate(commentData, {
         individualHooks: true,
         returning: true,
     });
